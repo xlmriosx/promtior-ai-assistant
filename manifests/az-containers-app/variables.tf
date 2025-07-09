7eef2f37868eb6cfee7026b782601a370ec485cc
c00rioslum <xlmriosx@gmail.com>
2025-07-09 00:12:07 -0300

    fix: push image other repo

--- manifests/az-containers-app/variables.tf
+++ manifests/az-containers-app/variables.tf
@@ -13,23 +13,23 @@ variable "location" {
 variable "frontend_image_name" {
   type        = string
   description = "Path image docker for use in backend"
-  default = "docker.io/shuhariko/chat-promtior-shuhariko-com"
+  default = "docker.io/xlmriosx/chat-promtior-shuhariko-com"
 }
 
 variable "frontend_image_tag" {
   type        = string
   description = "Frontend image tag"
-  default     = "c21e76f4322fee7392258e6f00597e9ddcea68c7"
+  default     = "last commit"
 }
 
 variable "backend_image_name" {
   type        = string
   description = "Path image docker for use in backend"
-  default = "docker.io/shuhariko/rag-promtior-shuhariko-com"
+  default = "docker.io/xlmriosx/rag-promtior-shuhariko-com"
 }
 
 variable "backend_image_tag" {
   type        = string
   description = "Backend image tag"
-  default     = "c21e76f4322fee7392258e6f00597e9ddcea68c7"
+  default     = "last commit"
 }
